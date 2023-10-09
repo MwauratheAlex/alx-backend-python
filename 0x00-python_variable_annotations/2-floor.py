167ae3533a550bb681668a9992c6ceed608b8d1d
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 11:36:22 +0300

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This module contains the function floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor - returns the floor of the float.
+    @n: float to floor
+    Return: floor of n
+    """
+    return math.floor(n)
