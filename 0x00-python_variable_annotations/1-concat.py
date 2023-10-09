bd8a1628971243f2819ca407287f3947455c639a
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 11:29:23 +0300

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This module contains the function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat - returns a concatenated string str1 + str2
+    @str1: first string
+    @string2: second string
+    Return: str1 + str2
+    """
+    return str1 + str2
