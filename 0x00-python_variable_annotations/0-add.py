91013b771e0b2e5ebbc8d831c137fb81297f041c
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 11:25:32 +0300

    0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This module contains the functon add"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add - adds two floats
+    @a: a float
+    @b: a float
+    Return: a + b
+    """
+    return a + b
