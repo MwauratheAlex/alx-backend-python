7cd27c97028c1a3c6f5d09dab29c3844083bb0b3
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 12:04:38 +0300

    6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""This module contains the functon sum_mixed_list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    sum_mixed_list - sums a list
+    @mxd_lst: list to sum
+    Return: sum of values in mxd_lst
+    """
+    return sum([n for n in mxd_lst])
