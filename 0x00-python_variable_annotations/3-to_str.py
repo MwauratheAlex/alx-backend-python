ade295685b3f348cb11eaaadfb1d874c07e674e3
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 11:40:08 +0300

    3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This module contains the function to_str"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str - converts float to string
+    @n: float to convert
+    Return: the string representation of the float n.
+    """
+    return str(n)
