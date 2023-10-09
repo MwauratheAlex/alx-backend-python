312f5b97f3927bab2820ab19d13dc4577833586d
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 12:58:38 +0300

    8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""This module contains the function make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    returns a function that multiplies a float by multiplier.
+    @multiplier: float
+    Return: function that multiplies a float by multiplier.
+    """
+    def multiplier_function(n: float) -> float:
+        return n * multiplier
+
+    return multiplier_function
