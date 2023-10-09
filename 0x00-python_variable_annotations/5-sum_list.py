51d80434d46b339b49cf62c01f2edc5b6991bf17
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-09 11:53:15 +0300

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This module contains the function sum_list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    sum_list - returns sum of floats in input_list as a float.
+    @input_list: list of floats
+    Return: sum of floats in input_list
+    """
+    return sum([n for n in input_list])
