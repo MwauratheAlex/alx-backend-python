595439ab58de00ef63f7cb440b9eee63ebf25b77
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-10 13:23:02 +0300

    0-basic_async_syntax.py

--- 0x01-python_async_function/theory/0-basic_async_syntax.py
+++ 0x01-python_async_function/theory/0-basic_async_syntax.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This modulw contains the function wait_random"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """wait_random that waits for a random delay between 0 and max_delay"""
+    delay = (random.random() * max_delay) + 1
+    await asyncio.sleep(delay)
+    return delay
