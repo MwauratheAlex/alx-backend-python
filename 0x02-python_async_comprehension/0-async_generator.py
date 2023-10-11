bb1ecc511681a5e834f6faf31eced4e4d9257ad6
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-11 11:40:36 +0300

    0-async_generator.py: pycodestyle

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,6 @@ import random
 from typing import Generator
 
 
-
 async def async_generator() -> Generator[float, None, None]:
     """loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10"""
