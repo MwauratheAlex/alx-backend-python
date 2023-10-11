f8ddaf81f0e87d9e520c4cbf6d0ffa05161d0abc
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-11 11:54:08 +0300

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""This module contains the coroutine async_comprehension"""
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """collects 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers."""
+    results = [result async for result in async_generator()]
+    return results
