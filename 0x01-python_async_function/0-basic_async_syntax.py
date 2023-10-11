1e2bc45ba7c6f42991d7aa5e84aea289f315d7b2
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-10-11 09:50:43 +0300

    1-concurrent_coroutines.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """wait_random that waits for a random delay between 0 and max_delay"""
-    delay: float = (random.random() * max_delay) + 1
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
