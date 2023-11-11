aaf6730cfbb66fa85f5449802358e092a4cd07f2
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-11 13:03:33 +0300

    2. Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,13 @@
 """ Mudule for testing utils.py"""
 from parameterized import parameterized
 from unittest import TestCase
-from utils import access_nested_map
+from unittest.mock import MagicMock, patch
+import utils
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(TestCase):
+    """ Tests for access_nested_map(nested_map, path)"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -20,8 +23,24 @@ class TestAccessNestedMap(TestCase):
         ({"a": 1}, ("a", "b"), "b")
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected_msg):
-        """ Tests if utils.access_nested_map exceptions"""
+        """ Tests utils.access_nested_map exceptions"""
         with self.assertRaises(KeyError) as context_manager:
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(context_manager.exception), repr(expected_msg))
+
+
+class TestGetJson(TestCase):
+    """ Tests for utils.get_json(url)"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests')
+    def test_get_json(self, test_url, test_payload,  mock_requests):
+        """ Tests utils.get_json()"""
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(get_json(test_url), test_payload)
