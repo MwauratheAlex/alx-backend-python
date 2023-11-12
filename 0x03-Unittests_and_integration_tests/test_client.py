b8763fb9a65bbd7821017ca75389e50f2b408047
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-11-12 10:22:02 +0300

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,34 +32,34 @@ class TestGithubOrgClient(TestCase):
         with patch(
                 'client.GithubOrgClient.org',
                 new_callable=PropertyMock
-                ) as mock_org:
+        ) as mock_org:
             mock_org.return_value = mock_payload
             test_client = GithubOrgClient(org_name=test_org_name)
             self.assertEqual(
-                    test_client._public_repos_url, mock_payload["repos_url"])
+                test_client._public_repos_url, mock_payload["repos_url"])
 
     @parameterized.expand([
-        ([{"name": "repo_1"}, {"name": "repo_2"}], "url_1"),
-        ([{"name": "repo_3"}, {"name": "repo_4"}], "url_2"),
-        ([{"name": "repo_5"}, {"name": "repo_6"}], "url_3"),
-        ([{"name": "repo_7"}, {"name": "repo_8"}], "url_4"),
+        ({
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [{"name": "episodes.dart", }, {"name": "kratu", }, ]
+        },),
     ])
     @patch('client.get_json')
-    def test_public_repos(self, test_payload, test_url, mock_get_json):
+    def test_public_repos(self, test_payload, mock_get_json):
         """  Unit-tests for  GithubOrgClient.public_repos"""
+        mock_get_json.return_value = test_payload["repos"]
         with patch(
                 'client.GithubOrgClient._public_repos_url',
                 new_callable=PropertyMock
-                ) as mock_public_repos_url:
+        ) as mock_public_repos_url:
 
-            mock_public_repos_url.return_value = test_url
-            mock_get_json.return_value = test_payload
+            mock_public_repos_url.return_value = test_payload["repos_url"]
 
             test_client = GithubOrgClient("test_org")
             test_public_repos = test_client.public_repos()
 
             expected_public_repos_return = [
-                repo["name"] for repo in test_payload
+                repo["name"] for repo in test_payload["repos"]
             ]
 
             self.assertEqual(test_public_repos, expected_public_repos_return)
